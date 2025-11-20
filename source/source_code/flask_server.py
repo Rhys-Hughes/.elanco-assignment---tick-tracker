eebc57b4a6143621dfad576ad6a580abab089d82
Rhys Hughes <rhysmichaelhughes1@gmail.com>
2025-11-20 01:52:36 +0000

    - created functioning search function
    - cleaned up the aggregation functions, it is proving surprisingly difficult to build these but progress is being made
    
    -built the system to populate the database with data
    
    - deleted the sightings.py file, the object orientation will not be particularly usefull
    
    - renamed the flask_handler to flask_server, more in line with it's actual function
    
    - added a direct database call function to the flask server, quite a dangerous function so will have to work on securing this

---
+++ source/source_code/flask_server.py
@@ -0,0 +1,105 @@
+from flask import Flask, request, jsonify
+import tick_tracker as tt
+
+app = Flask(__name__)
+
+# this is a basic connection test that shows connectivity with the database as well as showing a test variable passed in
+@app.route("/connection_test/<test_var>")
+def connection_test(test_var):
+    results = tt.connection_test(test_var)
+
+    return jsonify(results)
+
+
+
+# this is the search function, it asks for 2 terms
+# the search_term_dictionary is an associative array/dictionary which marks what fields and what values are desired
+# the search_condition is the logical condition used, for example AND, or OR
+# case sensitivity determines whether or not the case of the search conditions matters
+#
+# -- FURTHER INFORMATION IN THE DOCUMENTATION FILE --
+#
+@app.route("/search/<search_term_dictionary>/<search_condition>/<case_sensitivity>")
+def search(search_term_dictionary, search_condition, case_sensitivity):
+
+    # catches early errors like incorrectly formatted conditions
+    if (search_condition == "or" or search_condition == "and") and (case_sensitivity == "case sensitive" or case_sensitivity == "not case sensitive"):
+
+        # actually does the seach
+        results = tt.search(search_term_dictionary, search_condition, case_sensitivity)
+
+        if results == "error":
+            return jsonify({"ERROR" : "Failed call, ensure search_term_dictionary is correctly formatted"})
+        else:
+            return jsonify(results)
+        
+    else:
+        return jsonify({"ERROR" : "Failed call, please ensure that search_condition is 'or' or 'and', and that is case_sensitivity is 'case sensitive' or 'not case sensitive'"})
+
+
+
+# this is the word filter function, it asks for 2 terms
+# the filter_term_dictionary is a dictionary which marks what fields are filtered, and by what values
+# note that dates/times are valid here, from_date, from_time, to_date, and to_time are all accepted
+# the filter_condition marks whether this is an inclusionary or exclusionary filter
+#
+# -- FURTHER INFORMATION IN THE DOCUMENTATION FILE --
+#
+@app.route("/filter/<filter_term_dictionary>/<filter_condition>/<case_sensitivity>")
+def filter(filter_term_dictionary, filter_condition, case_sensitivity):
+    results = tt.filter(filter_term_dictionary, filter_condition,case_sensitivity)
+    return jsonify(results)
+
+
+
+# data aggregation
+# the functions which provide analytics to the frontend
+#
+# -- FURTHER INFORMATION IN THE DOCUMENTATION FILE --
+# 
+
+# calculates the metric per category
+# category is a dictionary in the form {"group" : data} - for example {"region" : "Manchester"}
+@app.route("/aggregate_metric_per_category/<metric>/<category>")
+def metric_per_category(metric, category):
+
+
+    if True:
+        results = tt.metric_per_category(metric, category)
+
+        if results == "error":
+            return jsonify({"ERROR" : "Failed call"})
+        else:
+            return jsonify(results)
+    
+    else:
+        return jsonify({"ERROR" : "Failed call, please ensure that metric is 'species' or 'sightings', and that the category is 'region' or 'species'. -- Please note: metric = 'species', category = 'species' together is not allowed. There is 1 species per species."})
+
+# calculates the metric over time
+@app.route("/aggregate_metric_over_time/<metric>/<time>")
+def metric_over_time(metric, time):
+    results = tt.metric_over_time(metric, time)
+    return jsonify(results)
+
+# calculates the metric per category over time
+@app.route("/aggregate_metric_per_category_over_time/<metric>/<category>/<time>")
+def metric_per_category_over_time(metric, category, time):
+    results = tt.metric_per_category_over_time(metric, category, time)
+    return jsonify(results)
+
+
+
+# allows a direct command to be passed to the database - dangerous
+# command is raw SQL that will be used to directly query the database
+#
+# -- FURTHER INFORMATION IN THE DOCUMENTATION FILE --
+# 
+@app.route("/command_database/<command>")
+def command_database(command):
+    results = tt.command_database(command)
+    return jsonify(results)
+
+
+
+if __name__ == "__main__":  
+    app.run(debug = True)
--- source/source_code/tick_tracker.py
+++ source/source_code/tick_tracker.py
@@ -1,4 +1,4 @@
-import database_manager as dbm, sighting as s
+import database_manager as dbm, ast
 
 # the manager code that will start the various functions
 
@@ -8,68 +8,141 @@ def tt_connect_to_database():
 
 
 
+# ========================================================================================================
+#                                               SEARCH
+# ========================================================================================================
+
+# search_term_dictionary : a dictionary pairing a data field with its desired value, eg {"location" : "Liverpool", "species" : "Marsh tick"}
+# case_sensitivity : either "case sensitive" or "not case sensitive", used to set the case_sensitivity_mark
+                    # case_sensitivity_mark : either "LOWER" or "", inserted into SQL to force it to ignore cases or not by either making everything
+                    #                         lower case, or leaving everything as it is
+# search condition : the condition that determines the relationship between the search terms, "and" or "or", for example, location = "Liverpool" AND species = "marsh tick"
+
+# checks the case sensitivity and sets the case_sensitivity_mark
+def case_sensitivity_check(search_term_dictionary, case_sensitivity):
+
+    # this is the statement which will be inserted into our SQL to make it lower case if need be
+    case_sensitivity_mark = ""
+
+    # apply case sensitivity rules
+    #   case sensitivity in SQL statements is denoted with LOWER() and UPPER()
+    #   for these purposes we will put everything in lower case if the case sensitivity is "case sensitive"
+    if case_sensitivity == "not case sensitive":
+        case_sensitivity_mark = "LOWER"
+
+        # going through the search term dictionary and making everything lower case
+        for key, term in search_term_dictionary.items():
+            search_term_dictionary[key] = term.lower()
+
+    return search_term_dictionary, case_sensitivity_mark
+
+
+def generate_SELECT_criteria(search_term_dictionary, search_condition, case_sensitivity_mark):
+    # what we will store unformatted criteria in
+    criteria_list = []
+
+    # each search term will be accounted for
+    for key, term in search_term_dictionary.items():
+
+        # this will generate a chunk of the SQL syntax, it could look something like:
+        # "LOWER(`location`) = LOWER('Liverpool')", or "(`location`) = ('Liverpool')" if there is case sensitivity
+        criteria_list.append(f"{case_sensitivity_mark}(`{key}`) = {case_sensitivity_mark}('{term}')")
+
+    # now we stitch together the individual criteria using the search_condition (and/or)
+    search_condition = search_condition.upper() # <-- this is mostly for formatting since SQL should be upper case
+
+    # to store our final output, starts with a space
+    criteria_string = " "
+    for i in range(len(criteria_list)):
+        # if not the final criteria, a the search condition "and" or "or" will be appended
+        if i != len(criteria_list) - 1:
+            criteria_string += f"{criteria_list[i]} {search_condition} "
+        
+        # if it is teh final criteria, no search condition will be appended
+        else:
+            criteria_string += f"{criteria_list[i]} "
+        
+    return criteria_string
+    
+
 # search the data set for time range and location, adding species too
-def search():
+def search(search_term_dictionary, search_condition, case_sensitivity):
     connection, cursor = tt_connect_to_database()
 
+    try:
+        # getting the search_term_dictionary into dictionary form
+        search_term_dictionary = ast.literal_eval(search_term_dictionary)
 
+        # creating the prerequisites to any case sensitivity enforcement
+        search_term_dictionary, case_sensitivity_mark = case_sensitivity_check(search_term_dictionary, case_sensitivity)
+
+        # generates a string which will indicate our criteria in the system
+        select_criteria = generate_SELECT_criteria(search_term_dictionary, search_condition, case_sensitivity_mark)
+
+        command = f"""
+                    SELECT * FROM `sightings` WHERE {select_criteria}; 
+                   """
+
+        results = dbm.command_database(cursor, command)
+
+        print(command)
+
+        return results
+
+    except Exception as e:
+        # a more accurate error message will be given by the flask_server
+        return "error"
+
+
+
+
+# ========================================================================================================
+#                                               FILTER
+# ========================================================================================================
 
 # filter out or filter in specific ranges of data, eg locations, times, etc
-def filter():
+def filter(filter_term_dictionary, filter_condition,case_sensitivity):
     connection, cursor = tt_connect_to_database()
 
 
 
-# debugging
+# ========================================================================================================
+#                                            AGGREGATION
+# ========================================================================================================
+# category is a dictionary formatted as {group : data}, for example, {"location" : "manchester"}
+
+
+# calculates the metric per category
+def metric_per_category(metric, category):
+    connection, cursor = tt_connect_to_database()
+
+
+
+# calculates the metric over time
+def metric_over_time(metric, time):
+    connection, cursor = tt_connect_to_database()
+
+
+
+# calculates the metric per category over time
+def metric_per_category_over_time(metric, category, time):
+    connection, cursor = tt_connect_to_database()
+
+
+
+# directly queries the database
+def command_database(command):
+    connection, cursor = tt_connect_to_database()   
+    results = dbm.command_database(cursor, command)
+    return results 
+
+
+# connection testing
 def connection_test(test_var):
     connection, cursor = tt_connect_to_database()
-    results = dbm.command_database(cursor, "SELECT * FROM `sightings`")
-    print(results)
     return {
-        "database_contents" : results,
+        "database_contents" : cursor,
         "test_variable" : test_var
     }
 
 
-
-
-
-
-
-
-
-
-
-# aggregate functions need refining
-
-# general data that can be retreived
-def aggregate_per():
-    connection, cursor = tt_connect_to_database()
-    # sightings per region
-
-    # sightings per species
-
-    # species per region
-
-
-
-# data over time is returned
-def aggregate_time_trends():
-    connection, cursor = tt_connect_to_database()
-    # trends over time, monthly/weekly
-        # find first instance and move along timeline by increment, tracking each metric
-
-        # sightings per region
-
-        # sightings per species
-
-        # species per region
-
-
-
-# counts the basic information desired
-def aggregate_count():
-    connection, cursor = tt_connect_to_database()
-    # all regions observed
-
-    # all species observed
